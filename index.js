00c2fca827055f4b5a459d4547415fa3e8cc0514
klpod221 <klpod221@gmail.com>
2023-06-14 15:06:03 +0700

    add rate limit

--- index.js
+++ index.js
@@ -41,6 +41,24 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(expressValidator());
 
+// rate limiter
+const rateLimit = require('express-rate-limit');
+const limiter = rateLimit({
+    windowMs: 1 * 60 * 1000, // 1 minute
+    max: 60, // 60 requests
+    handler: (req, res) => {
+        res.status(429).json({
+            message: 'Too many requests, please try again later.'
+        });
+    },
+    skip: (req, res) => {
+        if(req.ip === '::ffff:127.0.0.1')
+            return true;
+        return false;
+    }
+});
+app.use(limiter);
+
 // allow cross origin requests from the frontend
 app.use(cors({
     origin: process.env.FRONTEND_URL,
